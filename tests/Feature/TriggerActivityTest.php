177e2cc50c2d31ccb6caf34109422224d1e9534a
Eddy Rufino <eddyjaair@gmail.com>
2021-01-06 20:17:32 -0500

    Add relations morphs

--- tests/Feature/TriggerActivityTest.php
+++ tests/Feature/TriggerActivityTest.php
@@ -44,7 +44,12 @@ class TriggerActivityTest extends TestCase
 
     $this->assertCount(2, $project->activity);
 
-    $this->assertEquals('created_task', $project->activity->last()->description);
+    tap($project->activity->last(), function ($activity) {
+      $this->assertEquals('created_task', $activity->description);
+      $this->assertInstanceOf(Task::class, $activity->subject);
+      $this->assertEquals('Some task', $activity->subject->body);
+    });
+
   }
 
   /** @test */
@@ -61,7 +66,10 @@ class TriggerActivityTest extends TestCase
 
     $this->assertCount(3, $project->activity);
 
-    $this->assertEquals('completed_task', $project->activity->last()->description);
+    tap($project->activity->last(), function ($activity) {
+      $this->assertEquals('completed_task', $activity->description);
+      $this->assertInstanceOf(Task::class, $activity->subject);
+    });
   }
 
   /** @test */
